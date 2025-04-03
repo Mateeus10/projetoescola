1d00ceafa93a694970ffb7d0fa08c747a297ae56
Mateus Almeida Silva <89556345+Mateeus10@users.noreply.github.com>
2025-04-03 10:53:14 -0300

    Trocando a Base de dados

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,16 +1,13 @@
 "use strict";require('dotenv').config();
 
 module.exports = {
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  host: process.env.DATABASE_HOST,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+  dialect: 'sqlite',
+  storage: './pj_escola.db',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
   },
-
 };
